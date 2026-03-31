f205a81195de7aa1a23ce94bb11952a0df5903fe
Jeff Coster <dev@goldenroadgames.com>
2026-03-31 17:13:46 -0400

    Initial Commit

---
+++ Picture Postcard/Picture_PostcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Picture_PostcardApp.swift
+//  Picture Postcard
+//
+//  Created by Jeff  on 3/31/26.
+//
+
+import SwiftUI
+
+@main
+struct Picture_PostcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
